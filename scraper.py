f1a8b452b9bb2f5c919832bfcdb0fef851b33554
Dravya Gohil <57190083+dravya08@users.noreply.github.com>
2020-01-06 20:20:23 +0530

    Update scraper.py
    comment writing

--- scraper.py
+++ scraper.py
@@ -6,8 +6,12 @@ URL = 'http://amazon.in/Apple-iPhone-11-64GB-Purple/dp/B07XVL4P83?ref_=ast_sto_d
 
 headers = {
     "USER AGENT":'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36'}
-price = []
+
+price = ""          # price has to declared out function then make it global in the function
+title = ""         # title
 def check_price():
+    global price
+    global title
     page = requests.get(URL, headers=headers)
 
     soup = BeautifulSoup(page.content,'html.parser')
@@ -26,7 +30,7 @@ def check_price():
     replaced_price = stripped_price.replace(",","")
     find_dot = replaced_price.find(".")
     to_convert_price = replaced_price[0:find_dot]
-    converted_price = int(to_convert_price)
+    converted_price = to_convert_price #converted_price = int(to_convert_price) 
 
     print(converted_price)
 
